0a57add61ecfc65745d4360e14296523ba48bca9
Student <liskotov@bk.ru>
2023-10-30 21:10:44 +0300

    Задача решена

--- 1.py
+++ 1.py
@@ -1,6 +1,26 @@
-print("программа для решений квадратных уровнений")
-a = input("Введите число a: ")
-b = input("Введите число b: ")
-c = input("Введите число c: ")
+import math
 
-N = a + b
+print("программа для решений квадратных уровнений")
+bad_data = True
+while bad_data == True:
+    try:
+        a = int(input("Введите число a: "))
+        b = int(input("Введите число b: "))
+        c = int(input("Введите число c: "))
+        bad_data = False
+    except ValueError:
+        print("данные не привести к числу")
+
+D = (b * b) - (4 * a * c)
+print(D)
+
+if D > 0:
+    d = math.sqrt(D)
+    x1 = ((-b) + d) / (2 * a)
+    x2 = ((-b) - d) / (2 * a)
+    print(f"Уравнение имеет 2 корня. x1={x1},x2={x2}")
+elif D == 0:
+    x1 = (-b) / (2 * a)
+    print("Уравнение имеет 1 корнь. x1={}".format (x1))
+else:
+    print("Уравнение не имеет корней!")
