f768d788e5dff4ad2103d44f252f138096d6172e
Student <liskotov@bk.ru>
2023-11-02 19:20:48 +0300

    Создал файл users

---
+++ test.py
@@ -0,0 +1,10 @@
+a = range(1, 100)
+users = ["Ilya", "Kirill", "Oleg", "Pavel"]
+print(type(a))
+print(type(users))
+
+for element in a:
+    print(element * 15)
+
+for user in users:
+    print(f"Уважаемый {user}, добро пожаловать!!!")
