a43c680326e866281b42567f47562fa9a13c7f13
IlyaPlankin <Plankinover@gmail.com>
2023-11-07 20:57:44 +0300

    дописал файл put_data_to_db.py

--- put_data_to_db.py
+++ put_data_to_db.py
@@ -1,1 +1,8 @@
 import pymysql
+
+connection = pymysql.connect(host='10.10.101.161', user='pyuser', password='123456', database='test')
+
+cursor = connection.cursor()
+
+cursor.execute("insert into telsprav values('Ilya','Plankin','m','89975598656')")
+connection.commit()
