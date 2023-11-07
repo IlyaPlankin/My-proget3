cdf4bde681a1419cfe210aaaa40a316e2c338bc5
IlyaPlankin <Plankinover@gmail.com>
2023-11-07 22:53:47 +0300

    ДЗ решение №1

--- HomeWork.py
+++ HomeWork.py
@@ -1,6 +1,14 @@
-g = open("read_from_file.py", "r")
-
 import pymysql
+f = open("users.txt", "r")
+
+for line in f:
+    line = line.strip()
+    split_line = line.split(":")
+    name = split_line[0]
+    surname = split_line[1]
+    sex = split_line[2]
+    phone = split_line[3]
+f.close()
 
 connection = pymysql.connect(host='10.10.101.161', user='pyuser', password='123456', database='test')
 
