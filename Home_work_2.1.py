15e66e1a849048b8ec6bf68c1824e00c914bbf50
IlyaPlankin <Plankinover@gmail.com>
2023-11-13 00:02:15 +0300

    Создал файл Home_work_2.1.py

--- Home_work_2.1.py
+++ Home_work_2.1.py
@@ -1,5 +1,5 @@
-users = ('ilya: Plankin: m: 89996660544', 'Oleg: Galkin: m: 89996660544', 'Tamara: Karpova: f: 89996660554',
-              'Sveta: Bereskina: f: 89552896407')
+users = ('ilya:Plankin:m:89996660544', 'Oleg:Galkin:m:89996660544', 'Tamara:Karpova:f:89996660554',
+              'Sveta:Bereskina:f:89552896407')
 
 f = open('users2.txt', 'w')
 for user in users:
