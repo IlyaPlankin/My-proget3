5833b19b63588b698b801d1250372a8cdd278aa4
IlyaPlankin <Plankinover@gmail.com>
2023-11-16 18:58:54 +0300

    Создал файл Home_work_2.3.py Исправил домашку

--- Home_work_2.3.py
+++ Home_work_2.3.py
@@ -7,11 +7,13 @@ cursor.execute('select * from telsprav')
 
 data = cursor.fetchall()
 
+f = open('users2.txt', 'w')
+
 for elem in data:
     print(elem)
     name = elem[0]
     surname = elem[1]
     sex = elem[2]
     phone = elem[3]
-
-    print(name , phone)
+f.write(user + '\n')
+f.close()
