f105fbd0295b79d11ca3f05cb7374df5b79c6053
IlyaPlankin <Plankinover@gmail.com>
2024-07-11 18:20:29 +0300

    новое

--- api_echange_client.py
+++ api_echange_client.py
@@ -6,3 +6,6 @@ if __name__ == '__main__':
     in_valute_count = int(input("Сколько валюты вы хотите обменять! "))
 
     result=requests.get(f'http://192.168.214.109:8080/obmen/?val1={in_valute}&val2={out_valute}&count={in_valute_count}')
+
+
+print(result.text)
