bba4f4cefb16ac27a6d397f0acc02d0ab75b4b40
PlankinIS <Plankinover@gmail.com>
2024-08-29 11:54:04 +0300

    СОЗДАЛ ПРИМЕР

--- TELEGRAMBOT_2.py
+++ TELEGRAMBOT_2.py
@@ -1,4 +1,7 @@
 import telebot
+import requests
+
+from obmen_gui import result
 
 bot = telebot.TeleBot("7322048253:AAFnwogkCMgl_wBxoYaAqlyoeTXmgT2cmAo")
 
@@ -6,21 +9,23 @@ bot = telebot.TeleBot("7322048253:AAFnwogkCMgl_wBxoYaAqlyoeTXmgT2cmAo")
 @bot.message_handler(content_types=['text'])
 def welcome(pm):
     sent_msg = bot.send_message(pm.chat.id, "Прошу Вас написать валюту, которую хотите обменять (пример: USD)")
-    bot.register_next_step_handler(sent_msg, iv_handler)  # Next message will call the name_handler function
+    bot.register_next_step_handler(sent_msg, iv_handler)
 
 
 def iv_handler(pm):
     iv = pm.text
     sent_msg = bot.send_message(pm.chat.id, f"Вами была выбрана валюта {iv}, на какую валюту вы бы хотели ее обменять (пример: RUB)?")
-    bot.register_next_step_handler(sent_msg, ov_handler, iv)  # Next message will call the age_handler function
-
+    bot.register_next_step_handler(sent_msg, ov_handler, iv)
 
 def ov_handler(pm, iv):
     ov = pm.text
     bot.send_message(pm.chat.id, f"Ваша валюта {iv}, и вы ее хотите обменять на {ov}.")
-
-#def get_ovcount_from_api(iv, ov, count):
-    #apiurl = f'http://192.168.159.232:8080/obmen?val1={iv}&val2={ov}&count={count}'
-    #result = requests.get(apiurl)
+    bot.register_next_step_handler(f"На сегодняшний день вы получите{result}?")
 
 bot.polling()
+
+def get_ovcount_from_api(iv, ov, count):
+    apiurl = f'http://192.168.159.232:8080/obmen?val1={iv}&val2={ov}&count={count}'
+    result = requests.get(apiurl)
+
+
